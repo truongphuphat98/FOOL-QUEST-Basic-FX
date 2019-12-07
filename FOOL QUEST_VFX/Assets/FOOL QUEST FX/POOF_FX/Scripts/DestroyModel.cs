cf1a114cb1821c1f5c7ddda2f9449626feb251b0
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2019-12-07 19:01:51 +0100

    initial commit

---
+++ FOOL QUEST_VFX/Assets/FOOL QUEST FX/POOF_FX/Scripts/DestroyModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyModel : MonoBehaviour
+{
+    public float destroyTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, Time.deltaTime * destroyTimer);
+    }
+}
